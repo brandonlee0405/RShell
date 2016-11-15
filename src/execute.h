27e3247d86f8b9cd1078fe314912fd4ed27c3e6f
nseth001 <nseth001@ucr.edu>
2016-11-14 22:26:53 -0800

    Removed boost include

--- src/execute.h
+++ src/execute.h
@@ -6,11 +6,11 @@
 #include <stdio.h>
 #include <vector>
 #include <cstdlib>
-#include <sys/types.h> 
+#include <sys/types.h>
 #include <sys/wait.h>
-#include <sys/stat.h> 
+#include <sys/stat.h>
 #include <unistd.h>
-#include <boost/algorithm/string.hpp>
+//#include <boost/algorithm/string.hpp>
 
 
 using namespace std;
@@ -27,7 +27,7 @@ class Execute
     	    {
         		char *temp_arr[512];
         		memset(temp_arr, '\0', sizeof(temp_arr));
-    
+
         		if (separators.size() != 0)
         		{
         			if (i != 0)
@@ -35,12 +35,12 @@ class Execute
         				previous = separators.at(i - 1);
         			}
         		}
-        		
+
         		if (check_previous)
         		{
         			if (previous == ";")
         			{
-        				
+
         				for (unsigned hh = 0; hh < cmds.at(i).size(); ++hh)
         				{
         					temp_arr[hh] = const_cast<char *>(cmds.at(i).at(hh));
@@ -50,10 +50,10 @@ class Execute
         					exit_check = true;
         					return;
         				}
-    
+
         				pid_t PID = fork();
         				//pid_t PARENT;
-        				
+
         				if (PID < 0)
         				{
         					perror("Error Occurred\n");
@@ -69,9 +69,9 @@ class Execute
         						check_previous = false;
         					}
         				}
-    
+
         			}
-    
+
                     else if (previous == "&&")
                     {
                         for (unsigned hh = 0; hh < cmds.at(i).size(); ++hh)
@@ -85,13 +85,13 @@ class Execute
                         }
                         pid_t PID = fork();
                         //pid_t PARENT;
-                        
+
                         if (PID < 0)
                         {
                             perror("Error Occurred\n");
                             exit(-1);
                         }
-    
+
                         else if (PID == 0)
                         {
                             check_previous = true;
@@ -102,19 +102,19 @@ class Execute
                                 check_previous = false;
                             }
                         }
-    
+
                     }
                     else if (previous == "||")
                     {
                         check_previous = false;
                     }
-    
+
         		}
         		else
         		{
         			if (previous == ";")
         			{
-        				
+
         				for (unsigned hh = 0; hh < cmds.at(i).size(); ++hh)
         				{
         					temp_arr[hh] = const_cast<char *>(cmds.at(i).at(hh));
@@ -124,10 +124,10 @@ class Execute
         					exit_check = true;
         					return;
         				}
-    
+
         				pid_t PID = fork();
         				//pid_t PARENT;
-        				
+
         				if (PID < 0)
         				{
         					perror("Error Occurred\n");
@@ -143,14 +143,14 @@ class Execute
         						check_previous = false;
         					}
         				}
-    
+
         			}
-    
+
                     else if (previous == "&&")
                     {
                         check_previous = false;
                     }
-    
+
                     else if (previous == "||")
                     {
                         for (unsigned hh = 0; hh < cmds.at(i).size(); ++hh)
@@ -164,13 +164,13 @@ class Execute
                         }
                         pid_t PID = fork();
                         //pid_t PARENT;
-                        
+
                         if (PID < 0)
                         {
                             perror("Error Occurred\n");
                             exit(-1);
                         }
-    
+
                         else if (PID == 0)
                         {
                             check_previous = true;
@@ -185,8 +185,8 @@ class Execute
                 }
         	}
         }
-		 
-    
+
+
         void isExecute(vector<string> separators, vector<vector<char *> > cmds, bool & exit_check)
         {
         	string previous = ";";
@@ -194,11 +194,11 @@ class Execute
             string clear = "clear";
             bool check_previous = true;
             int temp_clear = 0;
-                    
+
             int command_size = cmds.size();
             int separator_size = separators.size();
             bool size_valid = true;
-                    
+
             if (separators.size() == 0)
             {
                 if (cmds.size() == 1)
@@ -207,10 +207,10 @@ class Execute
                     if (strcmp(exit1.c_str(), cmds.at(0).at(0)) == 0)
                     {
                         // this needs to exit the terminal
-                        exit_check = true;      
+                        exit_check = true;
                         return;
                     }
-                            
+
                     // Checks if there is clear
                     else if (strcmp(clear.c_str(), cmds.at(0).at(0)) == 0)
                     {
@@ -218,7 +218,7 @@ class Execute
                     }
                 }
             }
-        
+
             if (command_size == 0)
             {
                 if (separator_size != 0)
@@ -231,7 +231,7 @@ class Execute
                     return;
                 }
             }
-                    
+
             if (command_size <= separator_size)
             {
                 if (temp_clear != 1)
@@ -240,7 +240,7 @@ class Execute
                     return;
                 }
             }
-            
+
             if (size_valid)
             {
                 Execution(separators, cmds, exit_check, check_previous, previous, exit1);
