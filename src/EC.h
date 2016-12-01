8fa933a112a755cc72746bd7b6fbb2c83a8b0117
nseth001 <nseth001@ucr.edu>
2016-12-01 15:50:22 -0800

    Added prompt showing file path

--- src/EC.h
+++ src/EC.h
@@ -11,6 +11,9 @@ using namespace std;
 //extra credit getlogin and hostname prompt, pretty self explanatory
 void prompt(){
 	char* name = getlogin();
+	char* currPath = new char [512];
+	string currPath_s;
+	int positionHome;
 
 	if (name == NULL)
 	{
@@ -27,7 +30,21 @@ void prompt(){
 		cout << "Error: Cannot get login info." << endl;
 		exit(1);
 	}
-	cout << name << "@" << hostname << "$ ";
+
+	if (getcwd(currPath, 100) == NULL)
+	{
+		perror("Error");
+	}
+	currPath_s = currPath;
+	positionHome = currPath_s.find(getenv("HOME"));
+
+	if (positionHome == 0)
+	{
+		currPath_s.replace(positionHome, strlen(getenv("HOME")), "~");
+	}
+
+	cout << name << "@" << hostname << ":" << currPath_s.c_str() << "$ ";
+	delete[] currPath;
 
 }
 
